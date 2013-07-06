fc7a31ea660923b604f76962ff7074103a3cb685
Arun Prasad <akprasad@stanford.edu>
2013-07-05 18:04:52 -0700

    Add basic admin views

--- bodha/admin.py
+++ bodha/admin.py
@@ -1,6 +1,16 @@
 from flask import redirect
 from flask.ext.admin import Admin, BaseView, AdminIndexView, expose
+from flask.ext.admin.contrib.sqlamodel import ModelView as _ModelView
 from flask.ext.security import current_user
+
+from bodha import db
+from bodha import models
+
+
+class AuthMixin(object):
+
+    def is_accessible(self):
+        return current_user.has_role('admin')
 
 
 class AppIndexView(AdminIndexView):
@@ -14,8 +24,35 @@ class AppIndexView(AdminIndexView):
             return redirect('/')
 
 
+class AppView(AuthMixin, BaseView):
+    pass
+
+
+class ModelView(AuthMixin, _ModelView):
+    pass
+
+
+class UserView(ModelView):
+    column_exclude_list = form_excluded_columns = ('password',)
+
+
 admin = Admin(name='Index', index_view=AppIndexView())
 
+admin.add_view(ModelView(models.Project, db.session,
+                         category='Projects',
+                         name='Project list'))
+admin.add_view(ModelView(models.Segment, db.session,
+                         category='Projects',
+                         name='Segments'))
+admin.add_view(ModelView(models.Revision, db.session,
+                         category='Projects',
+                         name='Revisions'))
+admin.add_view(UserView(models.User, db.session,
+                        category='Auth',
+                        name='Users'))
+admin.add_view(ModelView(models.Role, db.session,
+                         category='Auth',
+                         name='Roles'))
 
 # Attach to app
 # -------------
