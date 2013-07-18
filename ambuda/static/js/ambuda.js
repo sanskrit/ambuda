c762d17eabfce9fd3778d8713769e7d95bfaec08
Arun Prasad <akprasad@stanford.edu>
2013-07-17 19:13:23 -0700

    Move textarea code to Backbone view

--- ambuda/static/js/ambuda.js
+++ ambuda/static/js/ambuda.js
@@ -3,117 +3,135 @@
         KEY_SPACE = 32,
         KEY_BACKSPACE = 8;
 
-    function hasDevanagari(data) {
+    // True iff the string ends in a Devanagari character.
+    function endsInDevanagari(data) {
         var mark = data.charCodeAt(data.length - 1);
+        // Devanagari [0x0900 - 0x097F]
         return (mark >= 0x0900 && mark <= 0x097F);
     }
 
-    function getCaretPosition(el) {
-        // Normal browsers
-        if (el.selectionStart !== undefined) {
-            return el.selectionStart;
-        }
+    // Binds a textarea to some useful helper functions.
+    ns.Textarea = Backbone.View.extend({
 
-        // IE
-        else if (document.selection) {
-            el.focus();
-            var range = document.selection.createRange();
-            range.moveStart('character', -el.value.length);
-            return range.text.length;
-        }
+        events: {
+            'keydown': 'transHelper'
+        },
 
-        else {
-            console.log('getCaretPosition: no support for browser');
-        }
+        // Get the caret position inside `el`.
+        getCaretPosition: function() {
+            var el = this.el;
 
-        return -1;
-    }
+            // Normal browsers
+            if (el.selectionStart !== undefined) {
+                return el.selectionStart;
+            }
 
-    function setCaretPosition(el, pos) {
-        // Normal browsers
-        if (el.setSelectionRange !== undefined) {
-            el.focus();
-            el.setSelectionRange(pos, pos);
-        }
+            // IE
+            else if (document.selection) {
+                el.focus();
+                var range = document.selection.createRange();
+                range.moveStart('character', -el.value.length);
+                return range.text.length;
+            }
 
-        // IE
-        else if (el.createTextRange) {
-            var range = el.createTextRange();
-            range.move('character', pos);
-            range.select();
-        }
+            else {
+                console.log('getCaretPosition: no support for browser');
+            }
 
-        else {
-            console.log('setCaretPosition: no support for browser');
-        }
-    }
+            return -1;
+        },
 
-    $.fn.beforeCaret = function(replacement) {
-        var el = $(this).get(0),
-            content = el.value,
-            pos = getCaretPosition(el);
-        if (replacement !== undefined) {
-            el.value = replacement + content.substr(pos);
-            setCaretPosition(el, replacement.length);
+        // Set the caret position.
+        setCaretPosition: function(pos) {
+            var el = this.el;
+
+            // Normal browsers
+            if (el.setSelectionRange !== undefined) {
+                el.focus();
+                el.setSelectionRange(pos, pos);
+            }
+
+            // IE
+            else if (el.createTextRange) {
+                var range = el.createTextRange();
+                range.move('character', pos);
+                range.select();
+            }
+
+            else {
+                console.log('setCaretPosition: no support for browser');
+            }
+
             return this;
-        } else {
-            return content.substring(0, pos);
-        }
-    };
+        },
 
-    ns.smartKeydown = function(e) {
-        var keyCode = e.keyCode;
-        // Space and backspace
-        if (keyCode === KEY_SPACE || keyCode === KEY_BACKSPACE) {
-            var $this = $(this),
-                before = $this.beforeCaret(),
-                tokens = before.split(' ');
+        // Get content before caret. If `replacement` is defined, replace
+        // the content with `replacement`.
+        beforeCaret: function(replacement) {
+            var content = this.el.value,
+                pos = this.getCaretPosition();
+            if (replacement !== undefined) {
+                this.el.value = replacement + content.substr(pos);
+                this.setCaretPosition(replacement.length);
+                return this;
+            } else {
+                return content.substring(0, pos);
+            }
+        },
 
-            if (tokens.length) {
-                var last = tokens[tokens.length - 1],
-                    changed = false;
+        transHelper: function(e) {
+            var keyCode = e.keyCode;
+            // Space and backspace
+            if (keyCode === KEY_SPACE || keyCode === KEY_BACKSPACE) {
+                var before = this.beforeCaret(),
+                    tokens = before.split(' ');
 
-                // Space
-                if (keyCode == KEY_SPACE && last.length > 1) {
-                    var index = last.indexOf('#');
-                    if (index != -1) {
-                        last = last.slice(0, index) + Sanscript.t(last.slice(index+1), INPUT, 'devanagari');
-                        changed = true;
-                    } else {
-                        index = last.indexOf('@');
+                if (tokens.length) {
+                    var last = tokens[tokens.length - 1],
+                        changed = false;
+
+                    // Space
+                    if (keyCode == KEY_SPACE && last.length > 1) {
+                        var index = last.indexOf('#');
                         if (index != -1) {
-                            var trans = Sanscript.t(last.slice(index+1), INPUT, 'iast');
-                            last = last.slice(0, index) + '[sa: ' + trans + ']';
+                            last = last.slice(0, index) + Sanscript.t(last.slice(index+1), INPUT, 'devanagari');
                             changed = true;
+                        } else {
+                            index = last.indexOf('@');
+                            if (index != -1) {
+                                var trans = Sanscript.t(last.slice(index+1), INPUT, 'iast');
+                                last = last.slice(0, index) + '[sa: ' + trans + ']';
+                                changed = true;
+                            }
                         }
                     }
-                }
 
-                // Backspace
-                else if (hasDevanagari(last)) {
-                    e.preventDefault();
-                    var prefix = '',
-                        regexp = /(\s)(\S*)$/,
-                        match = regexp.exec(last);
-                    if (match) {
-                        prefix = last.substring(0, match.index) + match[1];
-                        last = match[2];
+                    // Backspace
+                    else if (keyCode == KEY_BACKSPACE && endsInDevanagari(last)) {
+                        e.preventDefault();
+                        var prefix = '',
+                            regexp = /(\s)(\S*)$/,
+                            match = regexp.exec(last);
+                        if (match) {
+                            prefix = last.substring(0, match.index) + match[1];
+                            last = match[2];
+                        }
+                        last = prefix + '#' + Sanscript.t(last, 'devanagari', INPUT);
+                        changed = true;
                     }
-                    last = prefix + '#' + Sanscript.t(last, 'devanagari', INPUT);
-                    changed = true;
-                }
 
-                if (changed) {
-                    tokens[tokens.length - 1] = last;
-                    $this.beforeCaret(tokens.join(' '));
+                    if (changed) {
+                        tokens[tokens.length - 1] = last;
+                        this.beforeCaret(tokens.join(' '));
+                    }
                 }
             }
         }
-    };
+    });
 
 })(window.Ambuda = window.Ambuda || {});
 
 
 $(function() {
-    $('#text-input').keydown(Ambuda.smartKeydown);
+    new Ambuda.Textarea({ el: $('#text-input') });
 });
