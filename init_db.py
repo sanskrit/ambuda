d8810aca66bdc47ca323b274bd768f203a12e3e7
Arun Prasad <akprasad.public@gmail.com>
2014-06-06 22:36:47 -0700

    Fix init_db.py
    It's a small script, but one worth having. (?)

--- init_db.py
+++ init_db.py
@@ -1,24 +1,16 @@
-from ambuda import db
+from ambuda import create_app
 from ambuda.models import *
 
 
-def main():
+def populate_db():
     db.create_all()
 
-    if not Flag.query.count():
-        print 'Creating flags...'
-        names = ['submit', 'complete', 'hard', 'pass']
-        for name in names:
-            db.session.add(Flag(name=name))
 
-    if not Status.query.count():
-        print 'Creating statuses...'
-        names = ['incomplete', 'complete', 'verified']
-        for name in names:
-            db.session.add(Status(name=name))
-
+def main():
+    app = create_app(__name__, 'development.config')
+    with app.app_context():
+        populate_db()
     print 'Done.'
-    db.session.commit()
 
 
 if __name__ == '__main__':
